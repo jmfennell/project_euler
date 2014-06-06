9259b7bf500996a18d645f5d0a02b63fb25e1991
Jon Fennell <j.fennell@pinbellcom.co.uk>
2014-06-06 18:02:30 +0100

    Problem 4

---
+++ 004/script.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def is_pallindrome(i):
+	str_ = str(i)
+	str_rev = rev(str_)
+	return str_ == str_rev
+
+def rev(s):
+	return s[::-1]
+
+def main():
+	i=999
+	j=999
+	palindromes = []
+	for m in range(i,100,-1):
+		for n in range(j,100,-1):
+			if is_pallindrome(m * n):
+				palindromes.append(m*n)
+	print(max(palindromes))
+
+if __name__ == '__main__':
+    main()
