133146f6697fc17f509f94a2fb2c23bc041a3559
Jon Fennell <Jonathan.Fennell@egton.net>
2017-08-14 22:17:45 +0100

    Fix typo in 003

--- 003/script.py
+++ 003/script.py
@@ -8,7 +8,7 @@ def is_prime(i):
     largest = math.ceil(math.sqrt(i))
     for j in range(2, largest+1):
         if i % j == 0:
-            return False;
+            return False
     return True
 
 def main():
@@ -16,7 +16,7 @@ def main():
     for j in range(largest,0,-1):
         if num%j==0 and is_prime(j):
             print(j)
-            break;
+            break
 
 if __name__ == '__main__':
     main()
